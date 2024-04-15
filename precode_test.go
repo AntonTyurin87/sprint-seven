8229b324dcc70b8f9e868c47523838fd5fb24a91
Anton-Tyurin-AntonTyurin@gmail.com <AntonTyurin87@gmail.com>
2024-04-15 15:03:34 +0300

    Done three test function in precode_test.go

---
+++ precode_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// Запрос сформирован корректно, сервис возвращает код ответа 200 и тело ответа не пустое
+func TestMainHandlerWhenCorrect(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=4&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	body := responseRecorder.Body.String()
+
+	require.NoError(t, nil)
+	assert.Equal(t, responseRecorder.Code, http.StatusOK)
+	require.NotEmpty(t, body)
+}
+
+// Город, который передаётся в параметре city, не поддерживается. Сервис возвращает код ответа 400 и ошибку wrong city value в теле ответа.
+func TestMainHandlerWhenWrongCity(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=4&city=WrongCity", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	body := responseRecorder.Body.String()
+
+	require.NoError(t, nil)
+	assert.Equal(t, responseRecorder.Code, http.StatusBadRequest)
+	require.NotEmpty(t, body)
+	assert.Equal(t, "wrong city value", body)
+}
+
+// Если в параметре count указано больше, чем есть всего, должны вернуться все доступные кафе
+func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+	totalCount := 4
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	body := responseRecorder.Body.String()
+	list := strings.Split(body, ",")
+
+	require.NoError(t, nil)
+	assert.Equal(t, responseRecorder.Code, http.StatusOK)
+	require.NotEmpty(t, body)
+	assert.Equal(t, len(list), totalCount)
+}
